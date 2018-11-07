114780eaf18e073a44845b4ea89bb9bf07bf87bf
Mickaël BONNARD <44813941+tuxmika35@users.noreply.github.com>
2018-11-07 15:48:49 +0100

    Create Script_surveillance_cpu.sh
    Script qui va surveiller la charge système et vous envoyer un mail d'alerte si les valeurs prédéfinies dans le script viennent à être dépassées.
    
    Le load average ou charge système représente la charge moyenne de votre système sur une période de 1mn, 5mn et 15mn.
    
    Le load average est disponible via les commandes uptime ou bien encore /proc/loadavg.

---
+++ Script_surveillance_cpu.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Script de surveillance de la charge système
+# Licence MIT ( http://choosealicense.com/licenses/mit/ )
+# Auteur : Mickaël BONNARD
+
+# Variables
+mail="mail@admin.com"
+sujet="Charge CPU > 90% sur $HOSTNAME"
+
+compteur=0
+
+while true
+
+do
+
+# On attend 30 secondes entre chaque check
+
+sleep 30
+
+cpu_cores=`cat /proc/cpuinfo | grep 'processor' | wc -l`
+load_1min=`cat /proc/loadavg | awk '{print $1}'`
+charge=`echo $(($(echo $load_1min | awk '{print 100 * $1}') / $cpu_cores))`
+
+# si la charge est supérieure ou égale à 90 %
+
+if [ $charge -ge 90 ]
+
+ then
+
+# On incrémente alors le compteur de 1
+
+     compteur=$((compteur+1))
+
+# Si le compteur arrive à 3
+
+   if [ $compteur -eq  3 ]
+
+then
+
+top="$(ps -eo %cpu,pid,comm | sort -nr | head -3)"
+
+date=`date '+%d-%m-%Y %H:%M:%S'`   
+
+# Alors on envoie le mail d'alerte et on remet le compteur à 0
+
+    echo -e "$sujet\n\n" "$top" | mail -s "$date : $sujet" $mail
+
+compteur=0
+
+fi
+
+else
+
+# Si la charge n'est pas supérieure ou égale à 90 % alors le compteur est à 0
+
+compteur=0
+
+fi
+
+done
