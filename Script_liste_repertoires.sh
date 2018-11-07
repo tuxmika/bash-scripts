b48a3dbe27e47cd636a1d3b64cd41f0afe577636
Mickaël BONNARD <44813941+tuxmika35@users.noreply.github.com>
2018-11-07 15:17:48 +0100

    Create Script_liste_repertoires.sh
    Script  pour afficher les statistiques des dossiers ( liste, nombre, taille totale, taille par dossier...).
    
    Le script utilise simplement les commandes ls et du et me classe les dossiers par taille descendante en Mo

---
+++ Script_liste_repertoires.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash 
+# Licence MIT ( http://choosealicense.com/licenses/mit/ )
+# Script pour afficher les statistiques des dossiers ( liste, nombre, taille totale, taille par dossier...).
+# Auteur : Mickaël BONNARD 
+
+# Variables
+log='/var/log/liste.log' 
+sujet='Liste des repertoires dans /home' 
+emplacement='/home' 
+
+cd $emplacement 
+nombre=$(ls -d * | wc -l) 
+taille_totale=$( du -ch * | awk 'END {print $1}') 
+echo " Liste des repertoires situés dans /home (classement par taille):" > $log 
+echo "" >> $log 
+echo "nombre de repertoires : $nombre" >> $log 
+echo "taille totale : $taille_totale" >> $log 
+echo "" >> $log 
+du -sB M * | sort -nr >> $log 
+cat $log | mail -s "$sujet" -r mail@mail.fr
