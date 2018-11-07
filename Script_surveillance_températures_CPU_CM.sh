eb78c2f34f0eb217cd1c72a0e2bcee94154498bb
Mickaël BONNARD <44813941+tuxmika35@users.noreply.github.com>
2018-11-07 14:49:21 +0100

    Create Script_surveillance_températures_CPU_CM.sh
    Script qui va surveiller à intervalles réguliers la température du CPU et de la carte mère et vous envoyer un mail d'alerte si les valeurs prédéfinies dans le script viennent à être dépassées.
    
    Le script enverra un mail d'alerte pour un état "warning" et un autre si le statut passe à "critical".

---
+++ Script_surveillance_températures_CPU_CM.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Script surveillance températures CPU et carte mère
+# Licence MIT ( http://choosealicense.com/licenses/mit/ )
+# Auteur : Mickaël BONNARD
+# Pré-requis : lm-sensors
+
+# Variables
+warningcpu=20
+criticalcpu=29
+warningmb=20
+criticalmb=29
+sujetcpu="srvdebian : Alerte température CPU"
+sujetmb="srvdebian : Alerte température MB"
+mail=mail@mail.fr
+
+temp1=$(sensors | grep "CPU Temperature" | tr '+' ' ' | tr '.' ' ' | awk '{print $3}')
+
+    if [[ $temp1 -ge $warningcpu && $temp1 -lt $criticalcpu ]] ; then
+
+echo "Warning : La température CPU est à $temp1°C" | mail -s " $sujetcpu" $mail
+
+     elif [ $temp1 -ge $criticalcpu ] ; then
+
+echo "Critical : La température CPU est à $temp1°C" | mail -s " $sujetcpu" $mail
+
+fi
+
+temp2=$(sensors | grep "MB Temperature" | tr '+' ' ' | tr '.' ' ' | awk '{print $3}')
+
+        if [[ $temp2 -ge $warningmb && $temp2 -lt $criticalmb ]] ; then
+
+echo "Warning : La température MB est à $temp2°C" | mail -s " $sujetmb" $mail
+
+       elif [ $temp1 -ge $criticalmb ] ; then
+
+echo "Critical : La température MB est à $temp2°C" | mail -s " $sujetmb" $mail
+
+fi
