7bbc31021a0b7607ec91feb6f4b5b8f8a24239b4
Mickaël BONNARD <44813941+tuxmika35@users.noreply.github.com>
2018-11-07 13:39:00 +0100

    Script_surveillance_services.sh
    Un script  pour surveiller les services ( nom du service, PID et date de lancement du processus )...
    
    Le script utilise simplement les commandes ps et pgrep

---
+++ Script_surveillance_services.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script surveillance services
+# Licence MIT ( http://choosealicense.com/licenses/mit/ )
+# Auteur : Mickaël BONNARD
+
+liste="service1 service2 service3"
+for service in $liste
+do
+pgrep $service > /dev/null
+if [ $? -ne 0 ]
+then
+echo -e "\033[1;31mservice $service arreté\033[0m"
+else
+echo -e "\033[1;32mservice $service démarré\033[0m"
+ps -eo comm,pid,lstart | grep $service
+fi
+done
