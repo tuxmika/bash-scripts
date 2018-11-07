bda1143ecaf51fcca61f10e73c760b1987d8b19d
tuxmika <44813941+tuxmika35@users.noreply.github.com>
2018-11-07 08:15:53 +0100

    Notification_reboot_kernel_Centos.sh
    Pour déterminer qu'il doit vérifier les mises à jour pour une Debian ou une Centos, le script va d'abord vérifier la présence du fichier /etc/debian_version.
    
    Si celui-ci est bien présent, alors le script va vérifier et lister le nombre et le nom des paquets pouvant être mis à jour sur la Debian.
    
    Si le fichier au contraire n'est pas présent, le script vérifiera alors nombre et le nom des paquets pouvant être mis à jour sur la Centos.

---
+++ Notification_reboot_kernel_Centos.sh
@@ -0,0 +1,28 @@
+# Script notification redémarrage après un update de kernel sur Centos
+# Licence MIT ( http://choosealicense.com/licenses/mit/ )
+# Auteur : Mickaël BONNARD
+
+# Variables
+sujet="Redémarrage nécessaire sur $HOSTNAME"
+jour=$(date +'%d %B %Y')
+nouveau=$(ls -t /boot/vmlinuz-* | sed "s/\/boot\/vmlinuz-//g" | head -n1)
+actuel=$(uname -r)
+destinataire="mail@admin.com"
+expediteur="notifications_updates@admin.com"
+
+kernel(){
+echo -e "------------------------------------------------------------------------------------------------------\n"
+echo -e "\t$jour : Redémarrage nécessaire sur $HOSTNAME\n"
+echo -e "------------------------------------------------------------------------------------------------------\n"
+echo -e "Le kernel a été mis à jour, un redémarrage est nécessaire.\n\nkernel actuel : $actuel\nKernel installé : $nouveau\n"
+}
+
+if [ "$nouveau" != "$actuel" ];then
+
+kernel | mail -s "$sujet" -r "$expediteur" "$destinataire"
+
+else
+
+exit
+
+fi
