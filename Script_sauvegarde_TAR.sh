d7f3c53bae890c9124d3e53613a1dd3ac86c43a8
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2019-04-29 21:05:29 +0200

    Update Script_sauvegarde_TAR.sh

--- Script_sauvegarde_TAR.sh
+++ Script_sauvegarde_TAR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script sauvegarde complète/incrémentale via tar
 # Licence MIT ( http://choosealicense.com/licenses/mit/ )
-# Auteur : Mickaël BONNARD
+# Auteur : Mickaël BONNARD ( https://www.mickaelbonnard.fr )
 
 # Variables
 snapshot="emplacement du snapshot"
