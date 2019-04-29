1a4c0096bbba4cb0bb8e2bc32cc072b18b213a21
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2019-04-29 21:03:07 +0200

    Update Notification_reboot_kernel_Centos_Debian.sh

--- Notification_reboot_kernel_Centos_Debian.sh
+++ Notification_reboot_kernel_Centos_Debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script notification redémarrage après un update de kernel sur Centos et Debian.
 # Licence MIT ( http://choosealicense.com/licenses/mit/ )
-# Auteur : Mickaël BONNARD
+# Auteur : Mickaël BONNARD ( https://www.mickaelbonnard.fr )
 
 # Variables
 sujet="Redémarrage nécessaire sur $HOSTNAME"
