03fd9ac8152751b7d815e7217ee00844b5bc8f26
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2019-04-29 21:03:21 +0200

    Update Notifications_updates_Debian_Centos.sh

--- Notifications_updates_Debian_Centos.sh
+++ Notifications_updates_Debian_Centos.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # Script de notification de mises à jour Debian / Centos
 # Licence MIT ( http://choosealicense.com/licenses/mit/ )
-# Auteur : Mickaël BONNARD
+# Auteur : Mickaël BONNARD ( https://www.mickaelbonnard.fr )
 
 # Variables
 jour=$(date +'%d %B %Y')
