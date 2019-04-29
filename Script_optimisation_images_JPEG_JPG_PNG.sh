1cee3c9aa1e22f02ca16707d53ffaf282de05ee8
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2019-04-29 21:04:24 +0200

    Update Script_optimisation_images_JPEG_JPG_PNG.sh

--- Script_optimisation_images_JPEG_JPG_PNG.sh
+++ Script_optimisation_images_JPEG_JPG_PNG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script qui va permettre d'optimiser vos images ( .png, .jpeg et .jpg ).
 # Licence MIT ( http://choosealicense.com/licenses/mit/ )
-# Auteur : Mickaël BONNARD
+# Auteur : Mickaël BONNARD ( https://www.mickaelbonnard.fr )
 
 # Variables
 repertoire='/home/mickael/photos'
