a900ca7a09ebc9a9fc87db44fd97df0a4e4f8fd7
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2019-07-08 21:53:29 +0200

    Create Script_updates_Centos_v1.sh

---
+++ Script_updates_Centos_v1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Script mises à jour Centos 7
+# Le script va lister le nombre et le nom des paquets qui seront mis à jour.
+# Il va ensuite les installer et pour terminer envoyer un mail récapitulatif.
+# Licence MIT ( http://choosealicense.com/licenses/mit/ )
+# Auteur : Mickaël BONNARD ( https://www.mickaelbonnard.fr )
+
+liste=$(yum -q check-update | tail -n+2 | awk {'print $1'})
+nombre=$(yum -q check-update | tail -n+2 | wc -l)
+jour=$(date +'%d %B %Y')
+sujet="Mises à jour disponibles sur $HOSTNAME"
+destinataire="mail@admin.com"
+expediteur="notifications_updates@admin.com"
+
+yum check-update > /dev/null
+
+maj(){
+echo -e "------------------------------------------------------------------------------------------------------\n"
+echo -e "\t$jour : $nombre mises à jour disponibles sur $HOSTNAME\n"
+echo -e "------------------------------------------------------------------------------------------------------\n"
+for i in $liste;do
+actuelle=$(yum list installed $i | awk {'print $1,$2'} | tail -n+3)
+maj=$(yum -q check-update | tail -n+2 | awk {'print $1,$2'} | grep -w $i)
+echo -e "$i :\nInstallé : $actuelle\nCandidat : $maj\n"
+echo -e "------------------------------------------------------------------------------------------------------\n"
+done
+}
+
+if [ $nombre -ne 0 ];then
+
+maj | mail -s "$sujet" -r "$expediteur" "$destinataire"
+
+else
+
+exit
+
+fi
