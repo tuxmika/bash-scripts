74e9211689e2ead557bfb4cc4b904c21f5b6cc89
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2019-11-26 09:46:10 +0100

    Update Script_updates_Centos_v2.sh

--- Script_updates_Centos_v2.sh
+++ Script_updates_Centos_v2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script mises à jour Centos 7
+# Script mises à jour Centos 
 # Le script va lister le nombre et le nom des paquets qui seront mis à jour.
 # Il va ensuite les installer et pour terminer envoyer un mail récapitulatif.
 # Si un reboot est nécessaire suite à un upgrade de kernel , une notification sera inscrite en fin de mail.
