03b8fc524b03c9674cf8de007fd48a0d4b211728
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2020-01-16 11:04:19 +0100

    Update Script_sauvegarde_FTP.sh

--- Script_sauvegarde_FTP.sh
+++ Script_sauvegarde_FTP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 # Script de sauvegarde FTP
-# Licence MIT ( http://choosealicense.com/licenses/mit/ )
+# Licence CC BY-NC-SA 4.0 ( https://creativecommons.org/licenses/by-nc-sa/4.0/ )
 # Auteur : Mickaël BONNARD ( https://www.mickaelbonnard.fr )
 # Pré-requis : lftp ( http://lftp.yar.ru )
 
