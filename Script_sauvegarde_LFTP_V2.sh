a095392d4e4eab09ac835780a47777764d56f48c
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2020-01-27 09:47:51 +0100

    Update Script_sauvegarde_LFTP_V2.sh

--- Script_sauvegarde_LFTP_V2.sh
+++ Script_sauvegarde_LFTP_V2.sh
@@ -1,78 +1,120 @@
 #!/bin/bash
-# Script de sauvegarde complète/incrémentale avec LFTP
+
+#!/bin/bash 
+# Script de sauvegarde LFTP et du FTP sur SSL/TLS distant > local
 # Licence CC BY-NC-SA 4.0 ( https://creativecommons.org/licenses/by-nc-sa/4.0/ )
 # Auteur : Mickaël BONNARD ( https://www.mickaelbonnard.fr )
-# Pré-requis : lftp, MTA (Exim, Postfix, sSMTP...)
+# Pré-requis : lftp ( http://lftp.yar.ru )
 
-# Variables
-userftp=identifiant
-passftp=mot_de_passe
-hostftp=ip_serveur
-log=/home/user/sauvegardes
-jour=$(date +%d-%B-%Y)
+userftp=user
+passftp=password
+hostftp=00.11.22.33
+jour=$(date +%d-%m-%Y)
 heure=$(date +%H:%M:%S)
-local1="/home/user/dossier1" 
-local2="/home/user/dossier2" 
-distant=:/home/user/sauvegardes/ # ne pas oublier le slash à la fin du nom du répertoire distant
-mail="mail@admin.com"
+logs=/home/mickael/sauvegardes/logs
+log_ok=/home/user/sauvegardes/logs/sauvegarde_`date +%d-%m-%Y`
+log_ko=/home/user/sauvegardes/logs/sauvegarde_ko_`date +%d-%m-%Y`
+local="/home/user/sauvegardes"
+distant="wiki.user.fr"
+rotation=4 
 
-# manuel lftp : http://lftp.yar.ru/lftp-man.html
+# Si le dossier des logs n'existe pas, nous allons le créer.
+
+test -d $logs || mkdir -p $logs
+
+# Tout ce qui est OK sera écrit dans log_ok et tout ce qui est KO sera écrit dans log_ko.
+
+exec 1>$log_ok
+exec 2>$log_ko
+
+
+echo "-------------------------------------------------------------" 
+echo  "Sauvegarde de $distant du $(date +%d-%m-%Y)" 
+echo "-------------------------------------------------------------" 
+echo  "Début de la sauvegarde à `date +%T`" 
+echo "-------------------------------------------------------------" 
+
+# manuel lftp : https://lftp.yar.ru/lftp-man.html
 # userftp, passftp, hostftp : identifiants FTP.
 # -e de lftp avec des arguments entre guillemets : spécifie la commande ou les commandes à éxécuter
-# verbose : définit le niveau de verbosité.
 # mirror : Commande permettant d'effectuer une synchronisation entre un répertoire local et un répertoire distant.
-# -R de mirror : Permet la copie depuis l’emplacement local vers l’emplacement distant ( sans cette option, la copie s'effectue dans le sens distant > local ).
-# -e de mirror : Spécifie qu'il faut supprimer les fichiers distants s'ils n'existent plus dans l’emplacement local.
+# ftp:ssl-force true : On négocie une connexion SSL avec le serveur FTP.
+# ftp:ssl-protect-data true : On demande une connexion SSL pour les transferts de données.
+# ftp:ssl:verify-certificate no :  On désactive la vérification des certificats.
+# distant : Votre dossier distant.
 # local : Votre chemin local.
-# distant : Votre chemin distant ( si un slash est ajouté à la fin du nom du répertoire distant, alors votre répertoire local sera créé à l'intérieur du répertoire cible sur le serveur).
+# quit : coupe la connexion après le transfert.
 
-# Si le répertoire contenant les logs n'existe pas, celui-ci sera crée.
+# On lance la sauvegarde et la compression
 
-if [ ! -d $log ];then
+lftp "ftp://${userftp}:${passftp}@${hostftp}" -e "set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror $distant $local/sauvegarde_$jour; quit" 
 
-mkdir $log
+# Si la dernière commande s'est mal déroulée, on envoie un mail et on quitte le script.
 
-fi
+if [ "$?" -ne 0 ]
 
-# On teste avant tout si le serveur est accessible
-# Si celui-ci est inacessible, on l'inscrit dans le fichier de log, on envoie un mail et on quitte le script.
+then
 
-recus=$(ping -c $compteur $hostftp | grep 'received' | awk -F',' '{ print $2 }' | awk '{print $1 }') > /dev/null
+cat $log_ko | mutt -s "Erreur sur la sauvegarde de $distant du $jour" mail@mail.fr
 
-if [ $recus -eq 0 ];then
-
-echo -e "$jour-$heure : Serveur inaccessible.\n$hostftp : $compteur paquets transmis, $recus paquets reçus\n\nAucune sauvegarde effectuée." > $log/sauvegarde_$jour
-
-cat $log/sauvegarde_$jour | mail -s "Sauvegarde $jour" $mail
-
-exit
+exit 1
 
 fi
 
-# Si tout est OK, lancement de la sauvegarde.
+# On se place dans le répertoire local et on compresse le dossier.
 
-echo "Heure de demarrage de la sauvegarde : $(date +%H:%M:%S)" > $log/sauvegarde_$jour
+cd $local
 
-echo "" >> $log/sauvegarde_$jour
+tar -czvf sauvegarde_$jour.tar.gz sauvegarde_$jour 
 
-echo "Sauvegarde de $local1" >> $log/sauvegarde_$jour
+# Si la dernière commande s'est mal déroulée, on envoie un mail et on quitte le script.
 
-echo "" >> $log/sauvegarde_$jour
+if [ "$?" -ne 0 ]
 
-lftp "ftp://${userftp}:${passftp}@${hostftp}" -e "mirror --verbose=3  -R -e ${local1} ${distant} ; quit" >> $log/sauvegarde_$jour
+then
 
-echo "" >> $log/sauvegarde_$jour
+cat $log_ko | mutt -s "Erreur sur la compression de $distant du $jour" mail@mail.fr 
 
-echo "Sauvegarde de $local2" >> $log/sauvegarde_$jour
+exit 1
 
-echo "" >> $log/sauvegarde_$jour
+fi
 
-lftp "ftp://${userftp}:${passftp}@${hostftp}" -e "mirror --verbose=3  -R -e ${local2} ${distant} ; quit" >> $log/sauvegarde_$jour
+echo "-------------------------------------------------------------"
+echo  "Liste des sauvegardes avant rotation" 
+echo "-------------------------------------------------------------"  
 
-echo "" >> $log/sauvegarde_$jour
+ls -lrth *.tar.gz | awk {'print $6" "$7" "$9" "$5'} 
 
-echo "Heure de fin de la sauvegarde : $(date +%H:%M:%S)" >> $log/sauvegarde_$jour
+Une fois que la 4ème sauvegarde et la compression sont terminés, on effectue la rotation. 
 
-# Pour finir, on envoie un mail récapitulatif.
+nombre_sauvegardes=$(ls -1 $local/*.tar.gz | wc -l)
 
-cat $log/sauvegarde_$jour | mail -s "Sauvegarde $jour" $mail
+ancien=$(ls -1 $local/*.tar.gz | head -1)
+
+if [ $nombre_sauvegardes -eq $rotation ]
+
+then
+
+rm -rf $ancien
+
+fi
+
+echo "-------------------------------------------------------------" 
+echo  "Liste des sauvegardes après rotation" 
+echo "-------------------------------------------------------------" 
+
+ls -lrth *.tar.gz | awk {'print $6" "$7" "$9" "$5'} 
+
+# Si la compression et la rotation sont OK, alors on peut supprimer le dossier.
+
+rm -rf sauvegarde_$jour
+
+echo "-------------------------------------------------------------" 
+echo "Fin de la sauvegarde de $distant le $(date +%d-%m-%Y) à `date +%T`" 
+echo "-------------------------------------------------------------" 
+
+# On envoie un mail récapitulatif
+
+cat $log_ok | mutt -s "Sauvegarde de $distant du $jour" mail@mail.fr
+
+
