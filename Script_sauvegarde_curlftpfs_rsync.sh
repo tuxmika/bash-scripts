dd91da3dc1228bbb3b2f1f1bdba69a548bb92739
Mickaël BONNARD <44813941+tuxmika@users.noreply.github.com>
2020-11-07 18:10:54 +0100

    Update Script_sauvegarde_curlftpfs_rsync.sh

--- Script_sauvegarde_curlftpfs_rsync.sh
+++ Script_sauvegarde_curlftpfs_rsync.sh
@@ -5,37 +5,52 @@
 # Prérequis : fichier .netrc ( https://ec.haxx.se/usingcurl/usingcurl-netrc )
 
 jour=`date +%d-%B-%Y`
-point_montage="/home/user/ftp"
-local="/home/user/sauvegardes"
-distant="dossier.domaine.fr"
-logs="/home/user/sauvegardes/logs"
-log="/home/user/sauvegardes/logs/sauvegarde_`date +%d-%m-%Y`"
-rotation=4
-ftp="ftp.domaine.fr"
+point_montage="/media/user/backup/ftp"
+local="/media/user/backup/"
+logs=/media/user/backup/logs
+log=/media/user/backup/logs/sauvegarde_wiki_`date +%d-%m-%Y`
+rotation=11
+ftp="ftp.user.fr"
+
+# Si le répertoire de logs et point de montage n'existent pas, ils seront crées
 
 test -d $logs || mkdir -p $logs
 
 test -d $point_montage || mkdir -p $point_montage
 
-# Tout sera écrit dans /home/user/sauvegardes/logs/sauvegarde_`date +%d-%m-%Y`"
+# Tout sera écrit dans /media/user/backup/logs/sauvegarde_wiki_`date +%d-%m-%Y`
 
 exec > $log 2>&1
 
 echo "-------------------------------------------------------------" 
-echo  "Sauvegarde de $distant du $jour" 
+echo  "Sauvegarde de dossier du $jour" 
 echo "-------------------------------------------------------------" 
 echo "Début de la sauvegarde le $(date +%d-%m-%Y) à `date +%T`" 
 echo "-------------------------------------------------------------" 
 echo  "Montage du FTP distant"
 echo "-------------------------------------------------------------"
 
-curlftpfs $ftp $point_montage && echo montage terminé
+curlftpfs $ftp $point_montage && echo Montage terminé
 
 echo "-------------------------------------------------------------" 
 echo  "Sauvegarde Rsync"
 echo "-------------------------------------------------------------"
 
-rsync -az --stats $point_montage/$distant/ $local/$distant-`date +%d-%B-%Y`
+rsync -azXXX --stats $point_montage/dossier/ $local/dossier_`date +%d-%B-%Y`
+
+# Si des erreurs lors de la sauvegarde
+
+if [ "$?" -ne 0 ]
+
+then
+
+umount -lv $point_montage
+
+cat $log | s-nail -s "Sauvegarde de dossier du $jour" mail@mail.fr
+
+exit 1
+
+fi
 
 echo "-------------------------------------------------------------" 
 echo  "Démontage du FTP distant"
@@ -49,7 +64,21 @@ echo "-------------------------------------------------------------"
 
 cd $local
 
-tar -czf $distant-`date +%d-%B-%Y`.tar.gz $distant-`date +%d-%B-%Y` && echo compression terminée 
+tar -czf dossier_`date +%d-%B-%Y`.tar.gz dossier_`date +%d-%B-%Y` && echo Compression terminée 
+
+# Si des erreurs lors de la compression
+
+if [ "$?" -ne 0 ]
+
+then
+
+umount -lv $point_montage
+
+cat $log | s-nail -s "Sauvegarde de dossier du $jour" mail@mail.fr
+
+exit 1
+
+fi
 
 echo "-------------------------------------------------------------"
 echo  "Liste des sauvegardes avant rotation" 
@@ -57,7 +86,7 @@ echo "-------------------------------------------------------------"
 
 ls -lrth *.tar.gz | awk {'print $6" "$7" "$9" "$5'} 
 
-# Une fois que la 4ème sauvegarde et la compression sont terminés, on effectue la rotation. 
+# Une fois que la 11ème sauvegarde et la compression sont terminés, on effectue la rotation. 
 
 nombre_sauvegardes=$(ls -1 *.tar.gz | wc -l)
 
@@ -79,10 +108,10 @@ ls -lrth *.tar.gz | awk {'print $6" "$7" "$9" "$5'}
 
 # Si la compression et la rotation sont OK, alors on peut supprimer le dossier.
 
-rm -rf $distant-`date +%d-%B-%Y`
+rm -rf dossier_`date +%d-%B-%Y`
 
 echo "-------------------------------------------------------------"
 echo "Fin de la sauvegarde le $(date +%d-%m-%Y) à `date +%T`" 
 echo "-------------------------------------------------------------" 
 
-cat $log | s-nail -s "Sauvegarde de $distant du $jour" user@mail.fr
+cat $log | s-nail -s "Sauvegarde de dossier du $jour" mail@mail.fr
